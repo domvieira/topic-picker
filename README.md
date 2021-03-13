ad1f596d29d045850c877284f47d9ebcda175157
domvieira <dominic.vieira@suse.com>
2021-03-13 09:05:00 -0800

    initial release

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # topic-picker
-Topic Picker is a tool to help managers have better 1:1 conversations with their reports by jumpstarting the conversation
+Topic Picker is a tool to help managers have better 1:1 conversations with their reports by jumpstarting the conversation. Click the button, read the question, and discuss together!
+
+## How to add/change topics
+The catalog of topics is contained within the topics.js file, allowing you too add/remove/change any of the topics or categories. If you do change the categories, make sure to update the buttons accordingly.
+
+## How to run it
+* Clone the Repo
+* Open index.html in your web browser
+* Click buttons to get started! (that's it)
---
+++ topics.js
@@ -0,0 +1,25 @@
+var topics = {
+  "Manager Improvement": [
+    "What could I do as a manager to make your work easier?",
+    "What do you like about my management style? What do you dislike?",
+    "Would you like more or less direction from me on your work?",
+    "What could I do to make you enjoy your work more?",
+    "How can I better support you?",
+    "What would you like to know about me?",
+    "Is there a situation you'd like my help with?",
+    "What is something I could do better? What is a criticism you have for me?"
+  ],
+  "Self Improvement": [
+    "Do you feel challenged at work? Are you learning new things?",
+    "What area of the company would you like to learn more about?",
+    "What skills would you like to develop right now?",
+    "Who in the company would you like to learn from? What do you want to learn?",
+    "How do you prefer to receive feedback?",
+    "Do you feel you're getting enough feedback?",
+    "What's a recent situation you wish you handled differently? What would you change?",
+    "What additional training or education would you like?",
+    "Are there any roles in the company you'd like to learn more about?",
+    "What do you think are the key skills for your role? How would you rate yourself for each of them?",
+    "Is there an aspect of your job you would like more help or coaching?"
+  ]
+};
